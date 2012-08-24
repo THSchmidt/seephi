ae7011fab35b360f95228feee4a7ecb1d8034178
schmidt@bit.uni-bonn.de <schmidt@bit.uni-bonn.de@bce78332-6eab-4bd7-dbbd-efd966b710b5>
2012-08-24 13:58:08 +0000

    Adaptions for new hosting, updated contact information

--- trunk/general/index.php
+++ trunk/general/index.php
@@ -35,7 +35,7 @@ $mainObj->print_htmlhead();
       &nbsp;
     </td>
     <td class="maincontent">
-      <h1><a name="imprint" />Imprint</a></h1>
+      <h1>Contact</h1>
       <div style="font-weight:bold">SeePHI</div>
       <div>Converter for regular expressions<br />(Seefeld-Convention &lt;-&gt; PHI-BLAST/PROSITE-Nomenclature)</div>
 
@@ -47,56 +47,35 @@ $mainObj->print_htmlhead();
           <td colspan="2" style="font-weight:bold; padding:2px; vertical-align:top">Thomas Schmidt</td>
         </tr>
         <tr>
-          <td colspan="2" style="padding:2px; vertical-align:top">Student of Bioinformatics</td>
-        </tr>
-        <tr>
-          <td style="padding:2px; vertical-align:top">Email:</td>
-          <td style="padding:2px; vertical-align:top">
-            <a href="javascript:location='mailto:'+'seep'+ /* Just click to the link... */ 'hi@fh-bingen.de?subj' + 'ect=SeePHI - Contact'">seephi<span style="display:none;">-Remove this Item-</span>@<span style="display:inline;">fh-bingen.de</span>
+          <td colspan="2" style="padding:2px; vertical-align:top">
+            PhD student<br />
+            Computational Structural Biology<br />
+            Department of Life Science Informatics<br />
+            B-IT, LIMES-Institute<br />
+            University of Bonn
           </td>
         </tr>
         <tr>
           <td style="padding:2px; vertical-align:top">Postal address:</td>
-          <td style="padding:2px; vertical-align:top">Richard-Wagner-Stra&szlig;e 2<br />
-              DE-68165 Mannheim</td>
-        </tr>
-      </table>
-
-      <br />
-
-      <h3>FH Bingen - University of Applied Sciences</h3>
-      <table>
-        <tr>
-          <td style="padding:2px; vertical-align:top">Postal address:</td>
-          <td style="padding:2px; vertical-align:top">Berlinstra&szlig;e 107<br />
-              DE-55411 Bingen am Rhein</td>
-        </tr>
-        <tr>
-          <td style="padding:2px; vertical-align:top">Website:</td>
-          <td style="padding:2px; vertical-align:top"><a href="http://www.fh-bingen.de/">http://www.fh-bingen.de/</a></td>
-        </tr>
-      </table>
-
-      <br /><hr />
-
-      <h1><a name="contact" />Contact</a></h1>
-      <table>
-        <tr>
-          <td colspan="2" style="font-weight:bold; padding:2px; vertical-align:top">Thomas Schmidt</td>
+          <td style="padding:2px; vertical-align:top">Dahlmannstr. 2<br />
+              DE-53113 Bonn</td>
         </tr>
         <tr>
           <td style="padding:2px; vertical-align:top">Email:</td>
           <td style="padding:2px; vertical-align:top">
-            <a href="javascript:location='mailto:'+'thomas'+ /* Just click to the link... */ 's@fh-b' + 'ingen.de'">thomass<span style="display:none;">-Remove this Item-</span>@<span style="display:inline;">fh-bingen.de</span>
+            <a href="javascript:location='mailto:'+'schmi'+ /* Just click to the link... */ 'dt@bit.uni-bonn.de?subj' + 'ect=SeePHI - Contact'">schmidt<span style="display:none;">-Remove this Item-</span>@<span style="display:inline;">bit.uni-bonn.de</span>
           </td>
         </tr>
         <tr>
           <td style="padding:2px; vertical-align:top">Website:</td>
-          <td style="padding:2px; vertical-align:top"><a href="http://www.t-schmidt.name/">http://www.t-schmidt.name/</a></td>
+          <td style="padding:2px; vertical-align:top"><a href="http://www.fh-bingen.de/">http://csb.bit.uni-bonn.de/schmidt.html</a></td>
+        </tr>
+        <tr>
+          <td style="padding:2px; vertical-align:top">Project Page:</td>
+          <td style="padding:2px; vertical-align:top"><a href="http://www.fh-bingen.de/">http://code.google.com/p/seephi</a></td>
         </tr>
       </table>
 
-      <br />
     </td>
   </tr>
   <tr>
--- trunk/include/version.inc.php
+++ trunk/include/version.inc.php
@@ -1,7 +1,7 @@
 
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
   <tr>
-    <td style="color:#FFFFFF; font-size:11px">SeePHI v0.7beta<div style="color:#FFFFFF; font-size:9px">Date: 2008-09-15</div></td>
+    <td style="color:#FFFFFF; font-size:11px">SeePHI v1.0beta<div style="color:#FFFFFF; font-size:9px">Date: 2012-08-24</div></td>
   </tr>
 </table>
 
